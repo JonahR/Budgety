80f4ba61b8e15cf82039181733689a17dfa9845f
Jonah Rueb <ruebjonah@gmail.com>
2019-05-29 20:00:38 -0500

    input boxes are cleared after submission

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ var UIController = (function() {
         },
         
         addListItem: function(obj, type) {
-            var html, newHtml, element;
+            var html, newHtml, element, fields;
             
             // Create HTML string with placeholder text
             if(type === 'inc'){
@@ -100,6 +100,21 @@ var UIController = (function() {
             
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+        },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+            
+            fields = document.querySelectorAll(DOMStrings.inputDescription + ',' + DOMStrings.inputValue);  
+            
+            var fieldsArr = Array.prototype.slice.call(fields);
+            
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+            
+            fieldsArr[0].focus();
+            
         },
         
         getDOMStrings: function() {
@@ -138,6 +153,10 @@ var controller = (function(budgetCtrl, UICtrl) {
         
         // 3. Add the new item to the UI
         UICtrl.addListItem(newItem, input.type);
+        
+        // 4. Clear the fields
+        UICtrl.clearFields();
+        
         // 4. Calculate the budget
         
         // 5. Display the budget on the UI
