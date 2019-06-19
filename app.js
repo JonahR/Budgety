44f30b29f58a31edbd623dab040a6f92305f62da
Jonah Rueb <ruebjonah@gmail.com>
2019-06-18 19:58:06 -0500

    Delete function now removes item from data structure.

--- app.js
+++ app.js
@@ -63,6 +63,21 @@ var budgetController = (function() {
             
         },
         
+        deleteItem: function(type, id) {
+            
+            // ids = [1 2 4 6 8]
+            var ids = data.allItems[type].map(function(current) {
+                return current.id;
+            });
+            
+            index = ids.indexOf(id);
+            
+            if (index !== -1) {
+                data.allItems[type].splice(index, 1);
+            }
+            
+        },
+        
         calculateBudget: function() {
             
             // Calculate total income and expenses.
@@ -248,9 +263,10 @@ var controller = (function(budgetCtrl, UICtrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            ID = split[1];
+            ID = parseInt(splitID[1]);
             
             // 1. Delete item from the data structure.
+            budgetCtrl.deleteItem(type, ID);
             
             // 2. Delete item from user interface.
             
